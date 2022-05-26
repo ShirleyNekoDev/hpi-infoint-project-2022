797caa5bfdf4b42e7d5ab718628849e0e9e4471c
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2022-05-26 13:51:10 +0200

    configured Elasticsearch sink for Kafka topic  "trade-events"

--- connect/delete-config.sh
+++ connect/delete-config.sh
@@ -2,8 +2,14 @@
 
 KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
-BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
+CORPORATE_CONFIG=${3:-"$(dirname $0)/elastic-sink-corporate.json"}
+TRADE_CONFIG=${3:-"$(dirname $0)/elastic-sink-trade.json"}
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
-CONNECTOR_NAME=$(jq -r .name $BASE_CONFIG)
+# Remove corporate events connector
+CONNECTOR_NAME=$(jq -r .name $CORPORATE_CONFIG)
+curl -Is -X DELETE $KAFKA_CONNECT_API/$CONNECTOR_NAME
+
+# Remove trade events connector
+CONNECTOR_NAME=$(jq -r .name $TRADE_CONFIG)
 curl -Is -X DELETE $KAFKA_CONNECT_API/$CONNECTOR_NAME
--- connect/push-config.sh
+++ connect/push-config.sh
@@ -2,8 +2,14 @@
 
 KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
-BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
+CORPORATE_CONFIG=${3:-"$(dirname $0)/elastic-sink-corporate.json"}
+TRADE_CONFIG=${3:-"$(dirname $0)/elastic-sink-trade.json"}
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
-data=$(cat $BASE_CONFIG | jq -s '.[0]')
+# Create corporate events connector
+data=$(cat $CORPORATE_CONFIG | jq -s '.[0]')
+curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
+
+# Create trade events connector
+data=$(cat $TRADE_CONFIG | jq -s '.[0]')
 curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
