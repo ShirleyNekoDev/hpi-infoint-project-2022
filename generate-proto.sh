6a4fa0518e80a83970ff89ce910e80d501b135e4
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2022-07-06 22:10:18 +0200

    update protobufs

--- generate-proto.sh
+++ generate-proto.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 protoc --proto_path=proto --python_out=build/gen proto/student/academic/v1/rb_announcement.proto
-protoc --proto_path=proto --python_out=build/gen proto/student/academic/v1/company.proto
-protoc --proto_path=proto --python_out=build/gen proto/student/academic/v1/person.proto
-protoc --proto_path=proto --python_out=build/gen proto/student/academic/v1/trade.proto
+protoc --proto_path=proto --python_out=build/gen proto/student/academic/v1/rb_company.proto
+protoc --proto_path=proto --python_out=build/gen proto/student/academic/v1/rb_person.proto
+protoc --proto_path=proto --python_out=build/gen proto/student/academic/v1/ffb_trade.proto
+protoc --proto_path=proto --python_out=build/gen proto/student/academic/v1/ffb_company.proto
 
 protoc --proto_path=proto --descriptor_set_out=build/gen/student/academic/v1/all.fdset proto/student/academic/v1/*
