cc8ff30698599cbadcf368c49707cf128daae4d0
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2022-07-06 22:10:50 +0200

    update elastic sinks

--- connect/push-config.sh
+++ connect/push-config.sh
@@ -4,6 +4,10 @@ KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
-CONNECTOR_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
+CONNECTOR_CONFIG=${3:-"$(dirname $0)/default-elastic-sink.json"}
+data=$(cat $CONNECTOR_CONFIG | jq -s '.[0]')
+curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
+
+CONNECTOR_CONFIG=${3:-"$(dirname $0)/ffbcompany-elastic-sink.json"}
 data=$(cat $CONNECTOR_CONFIG | jq -s '.[0]')
 curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
