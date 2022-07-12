3045f5d72abc077564566180c0ce5cb5affc0cdf
Philipp Kaufmann <philippkaufmann2014@gmail.com>
2022-07-12 19:16:19 +0200

    volume entries in compose yml

--- sample_elastic_query.sh
+++
@@ -1,9 +0,0 @@
-curl -X GET "localhost:9200/_search?pretty" -H 'Content-Type: application/json' -d'
-{
-    "query": {
-        "match": {
-            "reference_id":"HRB 31499"
-        }
-    }
-}
-'
