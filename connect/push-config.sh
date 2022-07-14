5f23f19cfe7d084cb44be957768b7cff50aba2a1
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2022-07-14 20:04:17 +0200

    fix kafka connect company merging

--- connect/push-config.sh
+++ connect/push-config.sh
@@ -7,3 +7,7 @@ KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 CONNECTOR_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
 data=$(cat $CONNECTOR_CONFIG | jq -s '.[0]')
 curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
+
+CONNECTOR_CONFIG=${3:-"$(dirname $0)/elastic-sink-ffb-company.json"}
+data=$(cat $CONNECTOR_CONFIG | jq -s '.[0]')
+curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
